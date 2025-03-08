2a412197f2f0029f7bcac4ad63fdbff9d50778a4
mahmoudashraf01 <dahmamahmoud87@gmail.com>
2025-03-08 17:02:30 +0200

    adding preferred messages column

---
+++ lib/core/features/reaom_map/presentation/widgets/preferred_messages_column.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:road_map_mentor/core/features/reaom_map/presentation/widgets/prefrred_messages_list_view.dart';
+import 'package:road_map_mentor/core/utils/widgets/text.dart';
+
+class PreferredMessagesColumn extends StatelessWidget {
+  const PreferredMessagesColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Preferred Messages',
+            style: title2Bold,
+          ),
+          const SizedBox(height: 8),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return const PreferredMessagesListView();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
