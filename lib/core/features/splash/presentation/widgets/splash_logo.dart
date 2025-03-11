fd30050b27bea956fc9f073207d6c7a74c3ae456
Tasneem Mahmoud <elshahwytasneem@gmail.com>
2025-03-12 01:26:25 +0200

    Update splash_logo.dart

--- lib/core/features/splash/presentation/widgets/splash_logo.dart
+++ lib/core/features/splash/presentation/widgets/splash_logo.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/image/logo.png',
+      width: 120,
+      height: 120,
+    );
+  }
+}
 
