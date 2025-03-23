4e5dec55e41ffd47b84bd6585f0345e28e7f0c39
Tasneem Mahmoud <elshahwytasneem@gmail.com>
2025-03-24 00:51:29 +0200

    Create buildHeader.dart

---
+++ lib/core/features/default_home/widgets/buildHeader.dart
@@ -0,0 +1,56 @@
+Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Welcome to ',
+                      style: TextStyle(
+                        color: Color(0xFFF5EFFC),
+                        fontSize: 24,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w600,
+                        height: 1.17,
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Mentor AI ',
+                      style: TextStyle(
+                        color: Color(0xFF9860E4),
+                        fontSize: 24,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w600,
+                        height: 1.17,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 3),
+              Image.asset(
+                'image/image.png',
+                width: 40,
+                height: 40,
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            "Meet our expert AI mentors to gain valuable knowledge and experience.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
