b41d0a8ab566d8828765bf53165307aa0b4c5516
Tasneem Mahmoud <elshahwytasneem@gmail.com>
2025-04-12 20:10:56 +0200

    Add files via upload

--- lib/core/features/verification/screens/verification_screen.dart
+++ lib/core/features/verification/screens/verification_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import 'verification_code_screen.dart';
+import 'package:road_map_mentor/core/features/verification_code/screens/verification_code_screen.dart';
+
+import 'dart:ui';
 
 class VerificationScreen extends StatefulWidget {
   @override
   _VerificationScreenState createState() => _VerificationScreenState();
 }
 
-class _VerificationScreenState extends State<VerificationScreen> {
+class _VerificationScreenState extends State<VerificationScreen>  with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   TextEditingController emailController = TextEditingController();
   String? errorMessage;
 
@@ -48,98 +54,223 @@ class _VerificationScreenState extends State<VerificationScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: 0, end: -20).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: Color(0xFF2E1A47),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(top: size.height * 0.05, left: size.width * 0.06, right: size.width * 0.06),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+      backgroundColor: Color(0xFF110A2B),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 300,
+            left: 60,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+              child: Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF352250),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: -30,
+            right: -70,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF9860E4),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 100,
+            left: 200,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFF9860E4),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            right: 50,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+              child: Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF40174C)
+                ),
+              ),
+            ),
+          ),
+
+          // شاشة المحتوى مع الـ AppBar
+          Column(
             children: [
-              Image.asset("image/image.png", width: size.width * 0.2),
-              SizedBox(height: size.height * 0.03),
-
-              Text(
-                "Verification",
-                style: TextStyle(
-                  fontSize: size.width * 0.08,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back, color: Colors.white),
+                  onPressed: () => Navigator.pop(context),
                 ),
               ),
-              SizedBox(height: size.height * 0.01),
-
-              Text(
-                "Enter your email to send OTP code",
-                style: TextStyle(fontSize: size.width * 0.04, color: Colors.white70),
-              ),
-              SizedBox(height: size.height * 0.04),
-
-              TextField(
-                controller: emailController,
-                style: TextStyle(color: Colors.white),
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  hintStyle: TextStyle(color: Colors.white54),
-                  filled: true,
-                  fillColor: Color(0xFF3E2C5B),
-                  prefixIcon: Icon(Icons.email, color: Colors.white),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              if (errorMessage != null)
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    errorMessage!,
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ),
-
-              SizedBox(height: size.height * 0.04),
-
-              GestureDetector(
-                onTap: validateAndContinue,
-                child: Container(
-                  width: double.infinity,
-                  height: size.height * 0.07,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    gradient: LinearGradient(
-                      colors: [Color(0xFF7A4DB6), Color(0xFFDFCEF7), Color(0xFFF0E7FB)],
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: size.height * 0.05,
+                        left: size.width * 0.06,
+                        right: size.width * 0.06
                     ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Continue",
-                      style: TextStyle(
-                        fontSize: size.width * 0.05,
-                        color: Color(0xFF2E1A47),
-                        fontWeight: FontWeight.bold,
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: size.height * 0.02),
+                        Center(
+                          child: AnimatedBuilder(
+                            animation: _animation,
+                            builder: (context, child) {
+                              return Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Transform.translate(
+                                    offset: Offset(0, _animation.value),
+                                    child: Image.asset(
+                                      "image/image.png",
+                                      width: size.width * 0.15,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Positioned(
+                                    left: 63.24,
+                                    top: 85.30,
+                                    child: Container(
+                                      width: 39.52,
+                                      height: 6.27,
+                                      decoration: ShapeDecoration(
+                                        color: const Color(0x667A4DB6),
+                                        shape: OvalBorder(),
+                                      ),
+                                    ),
+                                  ),
+
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                        SizedBox(height: size.height * 0.03),
+                        Text(
+                          "Verification",
+                          style: TextStyle(
+                            color: const Color(0xFFF0E7FB),
+                            fontSize: 32,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                            height: 1.60,
+                          ),
+                        ),
+                        SizedBox(height: size.height * 0.01),
+                        Text(
+                          "Enter your email to send OTP code",
+                          style: TextStyle(fontFamily: 'Inter', color: Colors.white70),
+                        ),
+                        SizedBox(height: size.height * 0.04),
+                        TextField(
+                          controller: emailController,
+                          style: TextStyle(color: Colors.white),
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Color(0xCCF5EFFC),fontSize: 14, fontFamily: 'Inter'),
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: Icon(Icons.email, color: const Color(0xFFF5EFFC)),
+                            ),
+
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 1,
+                                color: const Color(0xFF605B6C),
+                              ),
+                            ),
+                          ),
+                        ),
+                        if (errorMessage != null)
+                          Padding(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Text(
+                              errorMessage!,
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        SizedBox(height: size.height * 0.04),
+                        GestureDetector(
+                          onTap: validateAndContinue,
+                          child: Container(
+                            width: double.infinity,
+                            height: size.height * 0.07,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              gradient: LinearGradient(
+                                colors: [Color(0xFF7A4DB6), Color(0xFFDFCEF7), Color(0xFFF0E7FB)],
+                              ),
+                            ),
+                            child: Center(child: Text("Continue", style: TextStyle(
+                              color: const Color(0xFF352250),
+                              fontSize: 16,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w600,
+                            ),)),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
